16923d265cf21d69316e51fceb11464359dcfc19
方糖 <xiaoheikeji@vip.qq.com>
2022-06-08 20:02:32 +0800

    Init

---
+++ My_Shiro/src/test/java/com/ft/my_shiro/MyShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.my_shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
