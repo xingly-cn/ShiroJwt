229b8aeeaff9862fe8dbbd2b1e66528a756c17e5
方糖 <xiaoheikeji@vip.qq.com>
2022-06-08 22:40:38 +0800

    Init

---
+++ ShiroJwt/src/test/java/com/ft/shirojwt/ShiroJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.shirojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
