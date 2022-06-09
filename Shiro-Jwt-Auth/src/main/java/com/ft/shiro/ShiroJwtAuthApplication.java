d31e492ccb4d0e68e12793877e2384f7c5a2dcaf
方糖 <xiaoheikeji@vip.qq.com>
2022-06-09 19:52:41 +0800

    Init

---
+++ Shiro-Jwt-Auth/src/main/java/com/ft/shiro/ShiroJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ft.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroJwtAuthApplication.class, args);
+    }
+
+}
